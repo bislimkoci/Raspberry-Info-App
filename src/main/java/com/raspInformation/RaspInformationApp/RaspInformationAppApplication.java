ab46f66fc9470af83315b9191505febf2a7a0707
Bislim Koci <bislimkoci01@gmail.com>
2025-01-05 01:16:33 +0100

    Initial commit

---
+++ src/main/java/com/raspInformation/RaspInformationApp/RaspInformationAppApplication.java
@@ -0,0 +1,13 @@
+package com.raspInformation.RaspInformationApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspInformationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspInformationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raspInformation/RaspInformationApp/RaspInformationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raspInformation.RaspInformationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspInformationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
